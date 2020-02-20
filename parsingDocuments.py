43237f3db0a4d1129d29f8e7be74f16ac96da764
Ana00000 <anaatanackovic98@gmail.com>
2020-02-20 01:38:05 +0100

    #parsiranje_skupa_HTML_dokumenata omoguceno parsiranje dokumenata,nakon odabira direktorijuma

---
+++ parsingDocuments.py
@@ -0,0 +1,27 @@
+import os
+import trie
+import parser
+
+
+def parsing(root_dir):
+    root = trie.TrieNode('*')
+    found = 0
+
+    for subdir, dirs, files in os.walk(root_dir, topdown=True):
+
+        for file in files:
+
+            if file.endswith('.html'):
+
+                found = 1
+                par = parser.Parser()
+                par.parse(os.path.join(subdir, file))
+
+                for word in par.words:
+                    trie.add(root, word.lower(), os.path.join(subdir, file))
+
+    if found == 0:
+        print("Ne postoji html dokument u korenskom direktorijumu: ", root_dir)
+        return False, root
+
+    return True, root
